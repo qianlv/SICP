de9af44cbb30044bfd346a4ecec55134e9e3970a
qianlv7 <qianlv7@qq.com>
2021-10-01 13:47:25 +0800

    filter-accumulate to impl product relatively prime

--- Chapter1/E1.33.rkt
+++ Chapter1/E1.33.rkt
@@ -10,13 +10,6 @@
 ; filter. Write filtered-accumulate as a procedure.
 ; Show how to express the following using filtered-accumulate:
 
-;  a. the sum of the squares of the prime numbers in the
-;     interval a tob (assuming that you have a prime?
-;     predicate already written)
-;  b. the product of all the positive integers less than n that
-;     are relatively prime ton (i.e., all positive integersi < n
-;     such that GCD(i,n) = 1).
-
 (define (filtered-accumulate filter combiner null-value
                              term a next b)
   (cond ((> a b) null-value)
@@ -54,9 +47,30 @@
       #f
       (= n (smallest-divisor n))))
 
+;  a. the sum of the squares of the prime numbers in the
+;     interval a tob (assuming that you have a prime?
+;     predicate already written)
 (define (sum-prime a b)
   (define (inc x) (+ x 1))
   (define (square x) (* x x))
   (filtered-accumulate prime? + 0 square a inc b))
 
 (sum-prime 1 11)
+
+;  b. the product of all the positive integers less than n that
+;     are relatively prime ton (i.e., all positive integersi < n
+;     such that GCD(i,n) = 1).
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (product-relatively-prime n)
+  (define (filter x) (= (gcd x n) 1))
+  (define (identity x) x)
+  (define (inc x) (+ x 1))
+  (filtered-accumulate filter * 1 identity 1 inc (- n 1)))
+
+(product-relatively-prime 5) ; 1 * 2 * 3 * 4 = 24
+(product-relatively-prime 6) ; 5
