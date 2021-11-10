2786e4b88209c32d6d00cfd44a217e7f9ce001fb
qianlv7 <qianlv7@qq.com>
2021-11-10 17:01:35 +0800

    Mapping over trees

---
+++ Chapter2/E2.30.rkt
@@ -0,0 +1,39 @@
+#lang sicp
+
+; Define a procedure square-tree analogous to the square-list procedure
+; of Exercise 2.21. That is, square-tree should behave as follows:
+
+; (square-tree
+;   (list 1
+;         (list 2 (list 3 4) 5)
+;         (list 6 7)))
+
+; (1 (4 (9 16) 25) (36 49))
+
+; Define square-tree both directly (i.e., without using any higher-order
+; procedures) and also by using map and recursion.
+
+(define (square x) (* x x))
+
+(define (square-tree-rec tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree-rec (car tree))
+                    (square-tree-rec (cdr tree))))))
+
+(square-tree-rec
+  (list 1
+        (list 2 (list 3 4) 5)
+        (list 6 7)))
+
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+           (square-tree-map sub-tree)
+           (square sub-tree)))
+       tree))
+
+(square-tree-map
+  (list 1
+        (list 2 (list 3 4) 5)
+        (list 6 7)))
